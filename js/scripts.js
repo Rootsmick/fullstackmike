ab96db314dab8dea732ff45655ece69675244e32
Mike <mike@MacBook-Pro-di-Mike.local>
2025-02-21 13:05:51 +0100

    1.0.4

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,51 @@
-document.addEventListener("DOMContentLoaded", function() {
-    $(".timeline-item").click(function() {
-        $(this).find(".timeline-content").slideToggle();
-    });
+document.addEventListener("DOMContentLoaded", function () {
+	$(".timeline-item").click(function () {
+		$(this).find(".timeline-content").slideToggle();
+	});
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-	document.getElementById("contact-form").addEventListener("submit", function(event) {
+document.addEventListener("DOMContentLoaded", function () {
+	document.getElementById("contact-form").addEventListener("submit", function (event) {
 		event.preventDefault();
-		
+
 		var nome = document.getElementById("nome").value.trim();
 		var cognome = document.getElementById("cognome").value.trim();
 		var email = document.getElementById("email").value.trim();
 		var messaggio = document.getElementById("messaggio").value.trim();
-		
+
 		if (nome === "" || cognome === "" || email === "" || messaggio === "") {
 			alert("Tutti i campi sono obbligatori!");
 			return;
 		}
-		
+
 		var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 		if (!emailPattern.test(email)) {
 			alert("Inserisci un'email valida!");
 			return;
 		}
-		
+
 		alert("Messaggio inviato con successo!");
 		document.getElementById("contact-form").reset();
+	});
+});
+
+function toggleDetails(card) {
+	let details = card.querySelector(".details");
+	details.style.display = details.style.display === "none" ? "block" : "none";
+}
+document.addEventListener("DOMContentLoaded", function () {
+	var backToTopElement = document.getElementById('backToTop');
+
+	window.addEventListener('scroll', function () {
+		if (window.scrollY > 50) {
+			backToTopElement.style.display = 'block'; // Mostra il bottone quando scorre oltre 50px
+		} else {
+			backToTopElement.style.display = 'none';  // Nasconde il bottone se è meno di 50px
+		}
+	});
+
+	backToTopElement.addEventListener('click', function (event) {
+		event.preventDefault();
+		window.scrollTo({ top: 0, behavior: 'smooth' }); // Torna su con scroll lento
 	});
 });
